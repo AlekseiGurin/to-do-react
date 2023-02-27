3b6ed42d3f30ccd15969d596720dea040dd2f3fa
Aleksei Gurin <dmb2009isr@bk.ru>
2023-02-27 14:29:40 +0700

    add work with store

--- src/store/slices/taskSlice.ts
+++ src/store/slices/taskSlice.ts
@@ -10,3 +10,18 @@ type TasksType = {
 
 const initialState: TasksType[] = [];
 
+export const taskSlice = createSlice({
+     name: 'tasks',
+     initialState,
+     reducers: {
+         addTask: (state, action:PayloadAction<TasksType>) => {
+            state.push(action.payload)
+        }
+     }
+ });
+
+export const { addTask } = taskSlice.actions;
+
+export const selectTasks = (state: RootState) => state.tasks;
+
+export default taskSlice.reducer;
