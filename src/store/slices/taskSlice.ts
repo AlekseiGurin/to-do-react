49c181ff581e8cf11f1b64a3de299a19f5297127
Aleksei Gurin <dmb2009isr@bk.ru>
2023-03-01 20:58:11 +0700

    work with store

--- src/store/slices/taskSlice.ts
+++ src/store/slices/taskSlice.ts
@@ -1,6 +1,16 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import type { PayloadAction } from '@reduxjs/toolkit'
-import type { RootState } from '../store';
+import type { RootState, AppThunk } from '../store';
+
+export const fetchTasks = createAsyncThunk(
+    'tasks/fetchTasks',
+    async () => {
+        const respons = await fetch('https://jsonplaceholder.typicode.com/todos');
+        const data = await respons.json();
+        return data;
+    }
+);
+
 
 type TasksType = {
     id: string;
@@ -8,16 +18,36 @@ type TasksType = {
     taskStartDate: number;
 }
 
-const initialState: TasksType[] = [];
+type TasksState = {
+    taskList: TasksType[],
+    status: 'idle' | 'loading' | 'failed'
+}
+
+const initialState: TasksState = {
+    taskList: [],
+    status: 'idle'
+};
 
 export const taskSlice = createSlice({
      name: 'tasks',
      initialState,
      reducers: {
          addTask: (state, action:PayloadAction<TasksType>) => {
-            state.push(action.payload)
+            state.taskList.push(action.payload)
         }
-     }
+     },
+    extraReducers: (builder => {
+        builder
+            .addCase(fetchTasks.pending, (state) => {
+                state.status = 'loading';
+            })
+            .addCase(fetchTasks.fulfilled, (state, action) => {
+                state.status = 'idle';
+            })
+            .addCase(fetchTasks.rejected, (state) => {
+                state.status = 'failed';
+            });
+    })
  });
 
 export const { addTask } = taskSlice.actions;
